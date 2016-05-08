25c25d5e831360200a29dea3915a28d5ed01e192
Miguel Hernandez <michelangelo93@gmail.com>
2016-05-08 22:11:15 +0100

    first version and sample style

---
+++ nvidia-led.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+styles="~/.config/nvidia-led/styles"
+
+set_brightness() {
+  nvidia-settings -a GPULogoBrightness=$1 > /dev/null 2>&1
+}
+
+test_value() {
+  if [ -z $3 ]; then
+    return 1
+  fi
+  [[ $3 -ge $1 && $3 -le $2 ]]
+}
+
+test_range() {
+  if [[ $3 != *"-"* ]]; then
+    return 1
+  fi
+  min=$(echo $3 | cut -d "-" -f1)
+  max=$(echo $3 | cut -d "-" -f2)
+  if $(test_value $1 $2 $min) && $(test_value $1 $2 $max); then
+    [ $min -le $max ];
+  else
+    return 1
+  fi
+}
+
+show_help() {
+  echo "nvidia-led: Awesome animations for NVIDIA GeForce GTX leds"
+  echo "Usage: nvidia-led [options] [values]"
+  echo -e "\nOptions:"
+  for animation in $(ls $styles); do
+    echo "  $(source $styles/$animation info)"
+  done
+  echo "  help                                  Print this help"
+  echo -e "\nValues:"
+  echo "  intensity     Light intensity of animation. Type: Numeric/Range [0-100]. E.g., 80, 10-50, 0-100"
+  echo "  interval      Interval time of animation. Type: Numeric [1-100]. E.g., 20, 100, 70"
+  exit 0
+}
+
+custom_style() {
+  if [[ $(ls $styles | grep $1.sh) ]]; then
+    source $styles/$1.sh $2 $3
+  else
+    echo "Unknown argument. Try: 'nvidia-led help'"
+  fi
+}
+
+# MAIN
+case "$1" in
+  flashing) source $styles/flashing.sh $2 $3 ;;
+  help) show_help ;;
+  *)  custom_style $1 $2 $3 ;;
+esac
---
+++ styles/flashing.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+animation() {
+  test_range 0 100 $1 || show_help
+  test_value 1 100 $2 || show_help
+  interval=$(echo "$(($2*2)) 100" | awk '{printf "%.2f", $1/$2}')
+
+  while true; do
+    set_brightness $max
+    sleep 0.1
+    set_brightness $min
+    sleep $interval
+  done
+}
+
+show_info() {
+  echo "flashing <intensity> <interval>       Flashing animation"
+}
+
+show_help() {
+  echo "Usage: nvidia-led flashing <intensity> <interval>"
+  echo -e "\nValues:"
+  echo "  intensity     Type: Range [0-100]. E.g., 20-80, 10-50, 0-100"
+  echo "  interval      Type: Numeric [1-100]. E.g., 20, 100, 70"
+  exit 0
+}
+
+case "$1" in 
+  help) show_help ;;
+  info) show_info ;;
+  *)  animation $1 $2 ;;
+esac
